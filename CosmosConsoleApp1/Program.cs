03f854ab8a81b56bfb011f712800aa72a6925b27
vedau62504 <53147334+vedau62504@users.noreply.github.com>
2020-07-25 08:18:48 -0400

    Add project files.

---
+++ CosmosConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CosmosConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
